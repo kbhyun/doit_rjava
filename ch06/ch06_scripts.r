111b7fad2906f2612bfdc6b113e8414f3cd2d895
kbhyun <sbshkb@gmail.com>
2020-01-19 16:56:39 +0900

    06 변수추가하기

--- ch06/ch06_scripts.r
+++ ch06/ch06_scripts.r
@@ -146,3 +146,35 @@ head(mpg)
 
 mpg <- left_join(mpg, fuel, by = "fl")
 head(mpg)
+
+midwest<-as.data.frame(ggplot2:: midwest)
+
+
+str(midwest)
+
+
+########### 160페이지
+
+midwest <- midwest %>% 
+  mutate(ratio_child = (poptotal-popadults)/poptotal*100)
+head(midwest)
+
+midwest %>%
+  arrange(desc(ratio_child)) %>% 
+  select(state, county, ratio_child) %>% 
+  head(5)
+
+
+midwest <- midwest %>% 
+  mutate(grade = ifelse(ratio_child >=40, "large",
+                        ifelse(ratio_child >=30, "middle", "small")))
+
+table(midwest$grade)         
+
+midwest %>% 
+  mutate(ratio_asian = (popasian/poptotal)*100) %>% 
+  arrange(ratio_asian) %>% 
+  select(state, county, ratio_asian) %>% 
+  head(10)
+  
+
